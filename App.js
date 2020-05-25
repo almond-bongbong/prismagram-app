e6d90b3c785bc6c48179070ad0db9fa51979654c
cmlee <bal.dongdongdong@gmail.com>
2020-05-25 21:59:59 +0900

    update auth

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import { Asset } from 'expo-asset';
-import { AsyncStorage, Text, View } from 'react-native';
+import { AsyncStorage } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { persistCache } from 'apollo-cache-persist';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo-hooks';
+import { ThemeProvider } from 'styled-components';
+import styles from './styles';
 import apolloClientOptions from './apollo';
+import NavController from './components/NavController';
+import { AuthProvider } from './AuthContext';
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
@@ -42,9 +46,11 @@ export default function App() {
 
   return loaded && client ? (
     <ApolloProvider client={client}>
-      <View>
-        <Text>Open up App.js</Text>
-      </View>
+      <ThemeProvider theme={styles}>
+        <AuthProvider>
+          <NavController />
+        </AuthProvider>
+      </ThemeProvider>
     </ApolloProvider>
   ) : (
     <AppLoading />
---
+++ AuthContext.js
@@ -0,0 +1,54 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { AsyncStorage } from 'react-native';
+import { AppLoading } from 'expo';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children, isLoggedIn: isLoggedInProp }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const defaultIsLoggedIn = await AsyncStorage.getItem('isLoggedIn');
+      setIsLoggedIn(defaultIsLoggedIn === 'true');
+    })();
+  }, []);
+
+  const logUserIn = async () => {
+    try {
+      await AsyncStorage.setItem('isLoggedIn', 'true');
+      setIsLoggedIn(true);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const logUserOut = async () => {
+    try {
+      await AsyncStorage.setItem('isLoggedIn', 'false');
+      setIsLoggedIn(false);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return isLoggedIn !== null ? (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        logUserIn,
+        logUserOut,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  ) : (
+    <AppLoading />
+  );
+};
+
+export default AuthContext;
+
+export const useIsLoggedIn = () => useContext(AuthContext).isLoggedIn;
+export const useLogIn = () => useContext(AuthContext).logUserIn;
+export const useLogOut = () => useContext(AuthContext).logUserOut;
